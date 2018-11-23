f1e4bde8670c98da288899a0d6b11bb176eba342
Castlecelts <jamesdrmundie@gmail.com>
2018-11-23 20:06:11 +0000

    css good delete flex-container-fixed to resolve

--- js/app.js
+++ js/app.js
@@ -2,8 +2,11 @@ document.addEventListener('DOMContentLoaded', () => {
   console.log('Hi World, DOM loading');
 
   // this has added event listener for when a form is submitted
-  const submit = document.querySelector('form');
+  const submit = document.querySelector('#hero-inputs');
   submit.addEventListener('submit', handlesubmit)
+
+  const submitBackground = document.querySelector('#form-background');
+  submitBackground.addEventListener('submit', handleBackgroundSubmit)
 })
 
 // end of DOM loaded listener - all handles below
@@ -16,6 +19,7 @@ const handlesubmit = function(){
   const name = this.heroName.value;
   const universe = this.heroUniverse.value;
   const role = this.heroRole.value;
+  const equipment = this.equipment.value;
 
   // create variable injection into HTML
   const inject = document.querySelector('#hero-list');
@@ -38,13 +42,25 @@ const handlesubmit = function(){
   newRole.classList.add('heroRole');
   newRole.textContent = role;
 
+  const newEquipment = document.createElement('div');
+  newEquipment.classList.add('equipment');
+  newEquipment.textContent = equipment;
+
   // attach child to hero div
   newHero.appendChild(newName);
   newHero.appendChild(newUniverse);
   newHero.appendChild(newRole);
+  newHero.appendChild(newEquipment);
 
   // attach hero to injection point
   inject.appendChild(newHero);
-
-
 };
+
+const handleBackgroundSubmit = function(){
+  event.preventDefault();
+  const background = this.backgroundChange.value;
+  document.body.style.background = background;
+  document.body.style.backgroundSize = cover;
+  document.body.style.backgroundColor = "#f3f3f3";
+
+}
